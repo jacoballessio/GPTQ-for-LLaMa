d565d2417bfcfaa8d57db56e6615f41989fc8535
jacoballessio <39074704+jacoballessio@users.noreply.github.com>
2023-05-02 14:31:15 -0700

    Update setup_cuda.py

--- setup_cuda.py
+++ setup_cuda.py
@@ -1,10 +1,21 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
+arch_list = ['sm_80']
+
+class custom_BuildExtension(cpp_extension.BuildExtension):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def build_extensions(self):
+        for extension in self.extensions:
+            extension.extra_compile_args['nvcc'] = ['-arch={}'.format(arch) for arch in arch_list]
+        super().build_extensions()
+
 setup(
     name='quant_cuda',
     ext_modules=[cpp_extension.CUDAExtension(
         'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu']
     )],
-    cmdclass={'build_ext': cpp_extension.BuildExtension}
+    cmdclass={'build_ext': custom_BuildExtension}
 )
